587f7740643238cc167ce21cdb79cb7de21f37d2
nandhini-art <119108115+nandhini-art@users.noreply.github.com>
2025-11-03 12:50:14 +0530

    Create Predictive_test.py

---
+++ Predictive_test.py
@@ -0,0 +1,14 @@
+# Simple Python Program: Add two numbers
+
+# Get input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Display the result
+print("The sum is:", sum)
+
+
+test
